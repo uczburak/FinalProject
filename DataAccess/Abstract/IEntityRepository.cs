a182b8e949d1e7a907dd8a8a5401dc2b17687574
uczburak <uczbrk@gmail.com>
2022-03-23 19:06:10 +0300

    Adding Entity Framework methods to the project

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    //Generic constraint where T:class 
+    //class: referans tip
+    //IEntity: IEntity veya IEntity implemente eden bir class olabilir
+    //new(): Interface classları new lenemez.
+    public interface IEntityRepository<T> where T:class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
